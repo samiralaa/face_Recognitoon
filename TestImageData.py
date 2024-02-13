adc205e2385dbb37f11e9de81153ec27ebb9c451
samiralaa <149927853+samiraAlaalzeer@users.noreply.github.com>
2024-02-13 23:54:23 +0200

    test

--- TestImageData.py
+++ TestImageData.py
@@ -2,11 +2,12 @@ import cv2 as cv
 import sys 
 
 # Read the image file
-img = cv.imread("dddd_face.jpg")
-img2 = cv.imread("test_face.jpg")
-data = [img,img2]
+img = cv.imread("ss_face.jpg")
+
 # Check if the image was successfully loaded
 if img is None:
     sys.exit("Could not read the image.")
-cv.imshow("Display window", img2)
+cv.imshow("Open OutPut", img)
 k = cv.waitKey(0)
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
